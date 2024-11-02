1f161ebf6c4d930569347ffe0c5eecad552fe299
alantbarlow <alantbarlow@gmail.com>
2024-11-02 07:21:34 +0000

    add support for generating a passcode

--- create-random/__main__.py
+++ create-random/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+from passcode import Passcode
 from password import Password
 
 def run_program():
@@ -13,14 +14,24 @@ def run_program():
   password_parser.add_argument("--no-special", "-s", action = "store_true", dest = "special", help = "Exclude special characters from the password")
   password_parser.add_argument("--no-uppercase", "-u", action = "store_true", dest = "uppercase", help = "Exclude uppercase characters from the password")
 
+  # The subparser for the 'passcode' command
+  passcode_parser = subparsers.add_parser("passcode", help = "Output a random passcode with a given number of digets")
+  passcode_parser.add_argument("length", type = int, choices = [4, 5, 6], help = "The desired passcode length (must be either 4, 5, or 6)")
+  passcode_parser.add_argument("--allow-consecutive", "-c", action = "store_true", dest = "consecutive", help = "Allow consecutive individual integers (0-9) in the passcode")
+  
   args = parser.parse_args()
 
-  if args.command == "password":
+  if args.command == "password": 
     password = Password(args.length, exclude_uppercase_chars = args.uppercase, exclude_special_chars = args.special, exclude_numbers = args.numbers)
     print(password.generate())
-    
+
+  elif args.command == "passcode":
+    passcode = Passcode(args.length, allow_consecutive_integers = args.consecutive)
+    print(passcode.generate())
+
   else:
     parser.print_help()
+  
 
 # If the file is being run directly, run the main function
 if (__name__ == "__main__"):
---
+++ create-random/passcode.py
@@ -0,0 +1,20 @@
+import random
+
+class Passcode:
+
+  def __init__(self, length:int, allow_consecutive_integers:bool = False):
+    if not (4 <= length <= 6):
+      raise ValueError("The length of the passcode must be between 4 and 6 characters inclusive")
+        
+    self.__length = length
+    self.__allow_consecutive = allow_consecutive_integers
+
+  def generate(self) -> str:
+    passcode = str(random.randint(0, 9))
+
+    while len(passcode) < self.__length:
+      next_digit = str(random.randint(0, 9))
+      if (self.__allow_consecutive) or (next_digit != passcode[-1]):
+        passcode += next_digit
+
+    return passcode

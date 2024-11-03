c587ab63dbf63542e4861a80a9a9f01431e99f79
alantbarlow <alantbarlow@gmail.com>
2024-11-03 06:50:22 +0000

    add password length validator to insure the argument contains the required length

--- create-random/parser.py
+++ create-random/parser.py
@@ -8,7 +8,7 @@ class Parser:
 
         # The subparser for the 'password' command
         password_parser = subparsers.add_parser("password", help = "Output a random password that includes uppercase, lowercase, and special characters and numbers.")
-        password_parser.add_argument("length", type = int, help = "The desired length of the password")
+        password_parser.add_argument("length", type = self.__password_length_validator, help = "The desired length of the password")
         password_parser.add_argument("--no-numbers", "-n", action = "store_true", dest = "numbers", help = "Exclude numbers from the password")
         password_parser.add_argument("--no-special", "-s", action = "store_true", dest = "special", help = "Exclude special characters from the password")
         password_parser.add_argument("--no-uppercase", "-u", action = "store_true", dest = "uppercase", help = "Exclude uppercase characters from the password")
@@ -17,6 +17,12 @@ class Parser:
         passcode_parser = subparsers.add_parser("passcode", help = "Output a random passcode with a given number of digets")
         passcode_parser.add_argument("length", type = int, choices = [4, 5, 6], help = "The desired passcode length (must be either 4, 5, or 6)")
         passcode_parser.add_argument("--allow-consecutive", "-c", action = "store_true", dest = "consecutive", help = "Allow consecutive individual integers (0-9) in the passcode")
+    
+    def __password_length_validator(self, value):
+        i = int(value)
+        if i < 8:
+            raise argparse.ArgumentTypeError("Value must be greater than or equal to 8")
+        return i
     
     def parse(self) -> argparse.Namespace:
         return self.__parser.parse_args()

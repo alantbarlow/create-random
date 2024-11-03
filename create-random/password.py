fcf53e7d7249389da984fa7075fa4b572022ad25
alantbarlow <alantbarlow@gmail.com>
2024-11-03 16:31:21 +0000

    decrease likelihood of consecutive character types and add option for restricting consecutive characters

--- create-random/password.py
+++ create-random/password.py
@@ -9,11 +9,13 @@ class Password:
         exclude_special_chars: bool = False,
         exclude_uppercase_chars: bool = False,
         exclude_numbers: bool = False,
+        allow_consecutive_chars: bool = False,
     ):
         if length < 8:
             raise ValueError("The length of the password must be at least 8 characters")
 
         self.__length = length
+        self.__allow_consecutive = allow_consecutive_chars
 
         self.__character_options: dict[str, list[str]] = {
             "lowercase": list(string.ascii_lowercase),
@@ -35,20 +37,27 @@ class Password:
             for key in self.__character_options
             if (self.__character_options[key] != [])
         ]
+        previously_chosen_char_type: str = "uppercase"
 
         while len(password) < self.__length:
             chosen_chars = []
             char_types: list[str] = available_char_types.copy()
+
+            # Decrease likelihood of consecutive character types by making a 30% chance to remove them as an option
+            if (len(char_types) > 1) and (random.random() <= 0.3):
+                char_types.remove(previously_chosen_char_type)
 
             # Decrease likelihood of special characters by making a 60% chance to remove them as an option
             if ("special" in char_types) and (random.random() <= 0.6):
                 char_types.remove("special")
 
             chosen_char_type: str = random.choice(char_types)
+            previously_chosen_char_type = chosen_char_type
 
             chosen_chars.extend(self.__character_options[chosen_char_type])
+            next_char: str = random.choice(chosen_chars)
 
-            if chosen_chars != []:
-                password += random.choice(chosen_chars)
+            if (self.__allow_consecutive) or (next_char != password[-1]):
+                password += next_char
 
         return password

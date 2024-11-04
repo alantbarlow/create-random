8c4248ce46edea0a17ae756f38bfcdd6e5a07904
alantbarlow <alantbarlow@gmail.com>
2024-11-04 17:53:45 +0000

    add argument for defining a path to a json file to customize special characters used and update output formatting

--- create-random/__main__.py
+++ create-random/__main__.py
@@ -8,10 +8,15 @@ def run_program():
     args = parser.parse()
 
     if args.command == "password":
+        
+        special_chars: list[str] = args.custom_special_list
+        if args.exclude_special:
+            special_chars = []
+
         password = Password(
             args.length,
             exclude_uppercase_chars=args.uppercase,
-            exclude_special_chars=args.special,
+            custom_special_chars=special_chars,
             exclude_numbers=args.numbers,
             allow_consecutive_chars=args.consecutive,
         )
@@ -24,6 +29,8 @@ def run_program():
     else:
         parser.print_help()
 
+    # Print 2 new lines at the end of the output
+    print("\n")
 
 # If the file is being run directly, run the main function
 if __name__ == "__main__":
--- create-random/parser.py
+++ create-random/parser.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 
 class Parser:
@@ -10,9 +11,12 @@ class Parser:
         password_parser = subparsers.add_parser("password", help = "Output a random password that includes uppercase, lowercase, and special characters and numbers.")
         password_parser.add_argument("length", type = self.__password_length_validator, help = "The desired length of the password")
         password_parser.add_argument("--no-numbers", "-n", action = "store_true", dest = "numbers", help = "Exclude numbers from the password")
-        password_parser.add_argument("--no-special", "-s", action = "store_true", dest = "special", help = "Exclude special characters from the password")
         password_parser.add_argument("--no-uppercase", "-u", action = "store_true", dest = "uppercase", help = "Exclude uppercase characters from the password")
         password_parser.add_argument("--allow-consecutive", "-c", action = "store_true", dest = "consecutive", help = "Allow consecutive individual characters in the password")
+        
+        special_char_group = password_parser.add_mutually_exclusive_group()
+        special_char_group.add_argument("--no-special", "-s", action = "store_true", dest = "exclude_special", help = "Exclude special characters from the password")
+        special_char_group.add_argument("--special-path", "-p", type = self.__json_path_to_list, dest = "custom_special_list", metavar = "JSON_FILE_PATH", help = "The path to a JSON file that contains a JSON array of special characters")
 
         # The subparser for the 'passcode' command
         passcode_parser = subparsers.add_parser("passcode", help = "Output a random passcode with a given number of digets")
@@ -25,8 +29,32 @@ class Parser:
             raise argparse.ArgumentTypeError("Value must be greater than or equal to 8")
         return i
     
+    def __json_path_to_list(self, path):
+        if not path.lower().endswith('.json'):
+            raise argparse.ArgumentTypeError("The JSON_FILE_PATH must be a path to a file that has the extension of '.json'")
+        
+        try:
+            with open(path, 'r') as file:
+                data = json.load(file)
+
+                # Raise error if the json data can not be parsed into a python list of strings
+                if not isinstance(data, list) or not all(isinstance(item, str) for item in data):
+                    raise argparse.ArgumentTypeError("The JSON file specified in the JSON_FILE_PATH must contain a single array of strings")
+                return data
+            
+        except json.JSONDecodeError as decode_error:
+            raise argparse.ArgumentTypeError("The JSON file specified in the JSON_FILE_PATH does not contain valid JSON data") from decode_error
+        except FileNotFoundError as file_error:
+            raise argparse.ArgumentTypeError("The JSON file specified in the JSON_FILE_PATH was not found at that location") from file_error
+
+    
     def parse(self) -> argparse.Namespace:
-        return self.__parser.parse_args()
+        print()
+        try:
+            return self.__parser.parse_args()
+        except SystemExit as exit:
+            print("\n")  # Print a newline after the error message
+            raise exit
 
     def print_help(self):
         self.__parser.print_help()
